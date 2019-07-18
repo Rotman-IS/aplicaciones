6dfdd7d79ae3557471620490c804f745c630dffc
oscartenesaca <oscartenesaca@192.168.1.4>
2019-07-17 23:02:02 -0500

    clase conductor y listado

--- src/main/java/ec/edu/ups/entidadNegocio/RutaEN.java
+++ src/main/java/ec/edu/ups/entidadNegocio/RutaEN.java
@@ -1,28 +1,8 @@
 package ec.edu.ups.entidadNegocio;
 
 public class RutaEN {
-	
-	//atributos de Ruta
-	private int codigo;
-	private String descripcion;
-	
-	
-	//Getters y Setters
-	public int getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	
-	private void agregarRuta() {
-		//metodo para agregar una ruta
-	}
+
+		
+		
 
 }
